7c94b685a72c70fb0b2ee3b4a4fab13fb6e125af
Mansi_Jagtap <jagtapmansi2312@gmail.com>
2026-01-30 22:57:58 +0530

    commit

---
+++ document_backend/app/models/profile_model.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, TIMESTAMP, func
+from app.database import Base
+
+class Profile(Base):
+    __tablename__ = "profile"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.current_timestamp())
